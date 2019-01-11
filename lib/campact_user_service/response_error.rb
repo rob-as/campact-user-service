a647cd3ed1f07b74f7973c44035295e57f4e7bd0
anero <anero79@gmail.com>
2019-01-11 14:32:44 -0300

    Include HTTP status and body on response error exception.
    Version bump to 1.0.6

--- lib/campact_user_service/response_error.rb
+++ lib/campact_user_service/response_error.rb
@@ -3,6 +3,7 @@ module CampactUserService
     attr_reader :status_code, :body
 
     def initialize(status_code, body)
+      super("Response error. Status code: #{status_code} - Body: #{body}")
       @status_code = status_code
       @body = body
     end
